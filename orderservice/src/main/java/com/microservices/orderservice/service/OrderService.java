80636d9faff5568f41996a3fb7618316f64acc58
vivek-alladi <vivek.alladi26@gmail.com>
2023-11-15 23:34:00 +0530

    create place order endpoint

---
+++ orderservice/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.microservices.orderservice.service;
+
+import com.microservices.orderservice.dto.request.OrderLineItemsDto;
+import com.microservices.orderservice.dto.request.OrderRequest;
+import com.microservices.orderservice.model.Order;
+import com.microservices.orderservice.model.OrderLineItems;
+import com.microservices.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToOrderLineItems)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToOrderLineItems(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+
+        return orderLineItems;
+    }
+}
