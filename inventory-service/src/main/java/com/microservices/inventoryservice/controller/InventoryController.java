84467cdd4d93216142884f86c023cacc58017665
vivek-alladi <vivek.alladi26@gmail.com>
2023-11-16 21:40:11 +0530

    is in stock endpoint inventory

---
+++ inventory-service/src/main/java/com/microservices/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package com.microservices.inventoryservice.controller;
+
+import com.microservices.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventorySevice;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventorySevice.isInStock(skuCode);
+    }
+}
