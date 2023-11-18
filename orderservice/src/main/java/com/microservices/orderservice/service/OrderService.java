98f34270f5ea23c29f4b9ec220f9c5b2c15950ca
vivek-alladi <vivek.alladi26@gmail.com>
2023-11-18 18:20:12 +0530

    add load balance and launch multiple instances of inventory configuration by editting configuration

--- orderservice/src/main/java/com/microservices/orderservice/service/OrderService.java
+++ orderservice/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -37,8 +37,8 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("sku-code", skuCodes).build())
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("sku-code", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
