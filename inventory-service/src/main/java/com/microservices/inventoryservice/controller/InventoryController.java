ee51463584262975f87aeb0462485c1fdc7e0a9a
vivek-alladi <vivek.alladi26@gmail.com>
2023-11-18 15:50:24 +0530

    modify inventory endpoint

--- inventory-service/src/main/java/com/microservices/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/microservices/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.microservices.inventoryservice.controller;
 
+import com.microservices.inventoryservice.dto.response.InventoryResponse;
 import com.microservices.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,9 @@ public class InventoryController {
 
     private final InventoryService inventorySevice;
 
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam("sku-code") List<String> skuCode) {
         return inventorySevice.isInStock(skuCode);
     }
 }
