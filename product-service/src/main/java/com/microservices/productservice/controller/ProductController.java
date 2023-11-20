a2fe53a38c2495364b24504eb57b70316487f2f9
vivek-alladi <vivek.alladi26@gmail.com>
2023-11-20 22:48:59 +0530

    implement distributed tracing

--- product-service/src/main/java/com/microservices/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/microservices/productservice/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.microservices.productservice.dto.request.ProductRequest;
 import com.microservices.productservice.dto.response.ProductResponse;
 import com.microservices.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductController {
 
     private final ProductService productService;
@@ -19,12 +21,14 @@ public class ProductController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
+        log.info("Create Product");
         productService.createProduct(productRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
+        log.info("Get all products");
         return productService.getAllProducts();
     }
 }
